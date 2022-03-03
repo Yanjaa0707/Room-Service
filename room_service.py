f26df274d6b18818bbeaef1a5c218c29e3fd6b06
Yanjaa0707 <yanjinlkham.g@asu.edu.mn>
2022-03-03 14:41:31 +0800

    added guests, and asked questions

--- room_service.py
+++ room_service.py
@@ -1,2 +1,14 @@
+guests = {'111':'Jay', '222':'Sunoo', '333':'Heeseung'}
+correct_info = False
 
-print ("HEYs")
+room = input ("Please, enter room number: ")
+if room in guests.keys():
+    correct_info = True
+else:
+    correct_info = False
+
+name = input ("Please, enter your name: ")
+
+if correct_info and guests[room].lower() == name.lower():
+    correct_info =  True
+else: correct_info = False
